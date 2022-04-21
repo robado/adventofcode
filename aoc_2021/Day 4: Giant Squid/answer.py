8b3551bb88f8a1cb48b1f8e92b53da514108f944
robado <ridal.robert@gmail.com>
2022-04-21 22:11:00 +0300

    started day 4 part 1....

---
+++ aoc_2021/Day 4: Giant Squid/answer.py
@@ -0,0 +1,34 @@
+# --- Day 4: Giant Squid ---
+def run():
+    print('--- Day 4: Giant Squid ---')
+    bingo_file = read_file()
+    get_bingo_numbers(bingo_file)
+    get_bingo_tables(bingo_file)
+    part1(bingo_file)
+
+def part1(bingo):
+    pass
+
+def part2():
+    pass
+
+def get_bingo_numbers(bingo):
+    print('get_bingo_numbers')
+    bingo_nums = bingo.readline().rstrip().split(',')
+    print(bingo_nums)
+    # return bingo_nums
+    
+def get_bingo_tables(bingo):
+    print('get_bingo_tables')
+
+    
+def read_file():
+    #file = open("input.txt", "r")
+    file = open("test.txt", "r")
+    #inputs = file.read().splitlines()
+    #inputs = file.read()
+    #return inputs
+    return file
+    
+if __name__ == "__main__":
+    run()
