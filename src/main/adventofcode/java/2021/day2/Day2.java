190330a305ad376d0928d7b7feaab95b5666efc0
robado <ridal.robert@gmail.com>
2022-05-04 20:17:29 +0300

    day 2 part 1...

--- src/main/adventofcode/java/2021/day2/Day2.java
+++ src/main/adventofcode/java/2021/day2/Day2.java
@@ -1,7 +1,6 @@
 package day2;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 
 import utils.Utils;
@@ -12,21 +11,34 @@ import utils.Utils;
 public class Day2 {
    public static void main(String[] args) {
        System.out.println("Day 2");
-        List<String> directions = Utils.stringToFile(new File("src/main/recources/2021-example/day2.txt"));
+        List<String> directions = Utils.stringToFile(new File("src/main/recources/2021/day2.txt"));
 
         System.out.println("Part 1: " + part1(directions));
         System.out.println("Part 2: " + part2(directions));
    } 
 
    public static int part1(List<String> directions){
-       System.out.println("part1");
-       System.out.println(Arrays.toString(directions.toArray()));
+       int horizontal = 0;
+       int depth = 0;
 
-    return 0;
+        for (int i = 0; i < directions.size(); i++) {
+            String[] splitted = directions.get(i).split(" ");
+            if (splitted[0].equals("forward")) {
+                horizontal +=  Integer.parseInt(splitted[1]);
+            }
+            if (splitted[0].equals("down")) {
+                depth +=  Integer.parseInt(splitted[1]);
+            }
+            if (splitted[0].equals("up")) {
+                depth -=  Integer.parseInt(splitted[1]);
+            }
+        }
+
+    return horizontal*depth;
    }
 
    public static int part2(List<String> directions) {
-
-    return 0;
+       // System.out.println(Arrays.toString(directions.toArray()));
+       return 0;
    }
 }
