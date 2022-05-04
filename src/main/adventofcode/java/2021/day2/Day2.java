2655edeacb2acde8ace381b127aaa17dfb18022b
robado <ridal.robert@gmail.com>
2022-05-04 22:54:02 +0300

    day 2 part 2...

--- src/main/adventofcode/java/2021/day2/Day2.java
+++ src/main/adventofcode/java/2021/day2/Day2.java
@@ -6,39 +6,60 @@ import java.util.List;
 import utils.Utils;
 
 /**
- * Day 2: Dive! 
+ * Day 2: Dive!
  */
 public class Day2 {
-   public static void main(String[] args) {
-       System.out.println("Day 2");
+    public static void main(String[] args) {
+        System.out.println("Day 2");
         List<String> directions = Utils.stringToFile(new File("src/main/recources/2021/day2.txt"));
 
         System.out.println("Part 1: " + part1(directions));
         System.out.println("Part 2: " + part2(directions));
-   } 
+    }
 
-   public static int part1(List<String> directions){
-       int horizontal = 0;
-       int depth = 0;
+    public static int part1(List<String> directions) {
+        int horizontal = 0;
+        int depth = 0;
 
         for (int i = 0; i < directions.size(); i++) {
             String[] splitted = directions.get(i).split(" ");
             if (splitted[0].equals("forward")) {
-                horizontal +=  Integer.parseInt(splitted[1]);
+                horizontal += Integer.parseInt(splitted[1]);
             }
             if (splitted[0].equals("down")) {
-                depth +=  Integer.parseInt(splitted[1]);
+                depth += Integer.parseInt(splitted[1]);
             }
             if (splitted[0].equals("up")) {
-                depth -=  Integer.parseInt(splitted[1]);
+                depth -= Integer.parseInt(splitted[1]);
             }
         }
 
-    return horizontal*depth;
-   }
+        return horizontal * depth;
+    }
 
-   public static int part2(List<String> directions) {
-       // System.out.println(Arrays.toString(directions.toArray()));
-       return 0;
-   }
+    public static int part2(List<String> directions) {
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+
+        for (int i = 0; i < directions.size(); i++) {
+            String[] splitted = directions.get(i).split(" ");
+            if (splitted[0].equals("forward")) {
+                horizontal += Integer.parseInt(splitted[1]);
+                depth += (aim * Integer.parseInt(splitted[1]));
+            }
+            if (splitted[0].equals("down")) {
+                aim += Integer.parseInt(splitted[1]);
+            }
+            if (splitted[0].equals("up")) {
+                aim -= Integer.parseInt(splitted[1]);
+            }
+        }
+
+        return multiply(horizontal, depth);
+    }
+
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
 }
